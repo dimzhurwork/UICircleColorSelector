63e93f2f90a4a54cebc97e08f22b075a8c42ad52
Dima <dima@imac.local>
2018-09-12 13:06:52 +0300

    Initial Commit

---
+++ UICircleColorSelector/UICircleColorSelector.h
@@ -0,0 +1,19 @@
+//
+//  UICircleColorSelector.h
+//  UICircleColorSelector
+//
+//  Created by Dima on 12/09/2018.
+//  Copyright © 2018 Dmitriy Zhuravlev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UICircleColorSelector.
+FOUNDATION_EXPORT double UICircleColorSelectorVersionNumber;
+
+//! Project version string for UICircleColorSelector.
+FOUNDATION_EXPORT const unsigned char UICircleColorSelectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UICircleColorSelector/PublicHeader.h>
+
+
